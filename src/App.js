8664495be51023300fba444e8b4f009a7149b5a8
Willem <37781854+FruitSapling@users.noreply.github.com>
2024-09-23 15:15:11 +0700

    Switch back to BrowserRouter, but implements 404. Doing this for cleaner links without #

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // App.js
-import React from 'react';
+import { React, useEffect } from 'react';
 import './index.css'; // Import global styles from Index.css
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate, useNavigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Work from './pages/Work';
 import About from './pages/About';
@@ -9,6 +9,17 @@ import Contact from './pages/Contact';
 import Project from './pages/Project';
 
 function App() {
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const redirectPath = params.get('redirect');
+    if (redirectPath) {
+      navigate(redirectPath); // Navigate to the desired path
+    }
+  }, [navigate]);
+
   return (
     <div>
       <Navbar />
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 // index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
-  <Router>
+  <BrowserRouter>
     <App />
-  </Router>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
