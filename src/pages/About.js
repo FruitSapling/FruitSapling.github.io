4e242205fa750ac4fc227874e01ae833888729b1
Willem <vangerwen.willem@gmail.com>
2025-03-21 13:06:07 +0000

    Update CV

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,23 +11,31 @@ const About = () => {
         <img src="/images/profile_picture.jpeg" alt="Profile" className="about-image" />
       </div>
 
-      {/* Right Side: Text Content */}
-      <div className="about-content">
-        <h1>About Me</h1>
-        <p>
-          I have proven experience uncovering and prioritizing real user needs 
-          through a wide range of research methods. My approach ensures that 
-          every design decision is backed by meaningful insights.
-        </p>
-        <p>
-          I excel at communicating these insights clearly to stakeholders through 
-          efficient storytelling, bridging the gap between research, design, and development. 
-          My background in Computer Science and experience as a developer allow me to 
-          create feasible, user-friendly designs that remove friction and enhance usability.
-        </p>
-        <Link to="/work" className="work-link">View My Work</Link>
-        <br></br>
-        <Link to="/cv" className="work-link">View My CV</Link>
+      {/* Right Side: Content (Title + Bio) */}
+      <div className="about-text-container">
+        <div className="about-content">
+          <h1>Willem van Gerwen</h1>
+          <p>
+            I'm a UX Designer with a software development
+            background, bridging the gap between
+            design and engineering. Strong in user
+            research and effective collaboration with
+            developers in agile teams to create
+            seamless, user-centered products.
+          </p>
+          <p>
+            I have proven experience uncovering and prioritizing real user needs 
+            through a wide range of research methods. My approach ensures that 
+            every design decision is backed by meaningful insights.
+          </p>
+        </div>
+
+        {/* Buttons Below Content */}
+        <div className="about-buttons">
+          <Link to="/work" className="work-link">View My Work</Link>
+          <br></br>
+          <Link to="/cv" className="work-link">View My CV</Link>
+        </div>
       </div>
     </div>
   );
